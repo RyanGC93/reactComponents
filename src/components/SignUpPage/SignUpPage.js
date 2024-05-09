3a6ed1c485abeaefa6f2279e10fe3eb9324fccbf
Ryan Conk <ryangconk@gmail.com>
2024-05-09 20:43:30 +0000

    added signup logic

--- src/components/SignUpPage/SignUpPage.js
+++ src/components/SignUpPage/SignUpPage.js
@@ -1,22 +1,58 @@
 import React from "react";
 import "./SignUpPage.css";
+import { NavLink, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../services/firebase";
 
 function SignUpPage() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/login");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        // ..
+      });
+  };
   return (
     <div className="signup-container">
-      <h2 className="signup-header">Sign Up</h2>
+      {/* <h2 className="signup-header">Sign Up</h2>
       <form className="signup-form">
         <input type="text" placeholder="Name" className="signup-input" />
-        <input type="email" placeholder="Email" className="signup-input" />
+        <input
+          type="email"
+          placeholder="Email"
+          label="Email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className="signup-input"
+        />
         <input
           type="password"
           placeholder="Password"
           className="signup-input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
         />
         <button type="submit" className="signup-button">
           Sign Up
         </button>
-      </form>
+      </form> */}
     </div>
   );
 }
