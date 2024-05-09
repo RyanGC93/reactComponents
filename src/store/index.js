2d1b5931e3e0431bbda66ab479802fd28f85b6a3
Ryan Conk <ryangconk@gmail.com>
2024-05-09 21:51:59 +0000

    added redux lib created store

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+// add session
+// add user events
+// remove deprecated code (createStore)
+
+const rootReducer = combineReducers({
+  session: sessionReducer,
+  userEvents: userEventsReducer,
+});
+
+let enhancer;
+
+if (process.env.NODE_ENV === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
