074ca3cd724639a1c257dceab77c2a8e959afa90
Ryan Conk <ryangconk@gmail.com>
2024-05-11 19:35:05 +0000

    cleaned more house

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import './styles.css'
-
 import LandingPage from './components/LandingPage/LandingPage'
-
 import React, {  Fragment } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-
 export default function App() {
-
   return (
     <BrowserRouter>
-      <AuthProvider>
         <Fragment>
           <Routes>
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <NavBar />
-                  <LandingPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/login" element={<LoginPage />} exact={true} />
-            <Route path="/signup" element={<SignUpPage />} exact={true} />
-            <Route path="*" element={<LandingPage />} />
+           <Route path="*" element={<LandingPage />} />
           </Routes>
         </Fragment>
-      </AuthProvider>
     </BrowserRouter>
   )
 }
